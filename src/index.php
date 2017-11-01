23d6beae6fbfc6aec422d1048c008ab5924899db
narkssos <narkssos@Kenneths-Mac-Pro.local>
2017-11-01 10:55:17 +0900

    ..

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<?php
+namespace WebModule;
+
+class Context {
+    private $siteID;
+    private $pageID;
+    public function __construct($siteID, $pageID) {
+        $this->siteID = $siteID;
+        $this->pageID = $pageID;
+    }
+
+    public function siteID() {
+        return $this->siteID;
+    }
+    public function pageID() {
+        return $this->pageID;
+    }
+}
+
+interface IComponent{
+    function render();
+}
+
+?>
